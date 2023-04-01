4ab42b68e782756833161bbe2375aae0dc6c0e6d
jinsk9268 <jinsk9268@gmail.com>
2023-04-02 08:08:15 +0900

    feat: 로그인 관련 에러 코드 추가
    - ErrorCode

--- user-api/src/main/java/com/zerobase/commerce/user/exception/ErrorCode.java
+++ user-api/src/main/java/com/zerobase/commerce/user/exception/ErrorCode.java
@@ -7,11 +7,18 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
+  /**
+   * 회원가입, 인증
+   */
   ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
   NOT_FOUND_USER(HttpStatus.NOT_FOUND, "일치하는 회원이 없습니다."),
   ALREADY_VERIFY(HttpStatus.BAD_REQUEST, "이미 인증이 완료된 회원입니다"),
   WRONG_VERIFICATION(HttpStatus.BAD_REQUEST, "인증 코드가 일치하지 않습니다. 잘못된 인증 시도 입니다."),
-  EXPIRE_CODE(HttpStatus.BAD_REQUEST, "인증 시간이 만료되었습니다.");
+  EXPIRE_CODE(HttpStatus.BAD_REQUEST, "인증 시간이 만료되었습니다."),
+  /**
+   * 로그인
+   */
+  SIGNIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "아이디나 패스워드를 다시 확인해주세요.");
 
   private final HttpStatus httpStatus;
   private final String errorMessage;
