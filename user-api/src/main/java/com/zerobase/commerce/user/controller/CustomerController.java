b7c6007a07756d6a67d9723c71da8593f2b62798
jinsk9268 <jinsk9268@gmail.com>
2023-04-02 18:21:34 +0900

    feat: customer의 잔액 변경 POST API 요청 추가
    - CustomerController

--- user-api/src/main/java/com/zerobase/commerce/user/controller/CustomerController.java
+++ user-api/src/main/java/com/zerobase/commerce/user/controller/CustomerController.java
@@ -2,15 +2,19 @@ package com.zerobase.commerce.user.controller;
 
 import static com.zerobase.commerce.user.exception.ErrorCode.NOT_FOUND_USER;
 
+import com.zerobase.commerce.user.domain.customer.ChangeBalanceForm;
 import com.zerobase.commerce.user.domain.customer.CustomerDto;
 import com.zerobase.commerce.user.domain.model.CustomerEntity;
 import com.zerobase.commerce.user.exception.CustomException;
+import com.zerobase.commerce.user.service.customer.CustomerBalanceService;
 import com.zerobase.commerce.user.service.customer.CustomerService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import com.zerobase.domain.domain.common.UserVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,7 @@ public class CustomerController {
 
   private final JwtAuthenticationProvider provider;
   private final CustomerService customerService;
+  private final CustomerBalanceService customerBalanceService;
 
   @GetMapping("/info")
   public ResponseEntity<CustomerDto> getCustomerInfo(
@@ -34,4 +39,15 @@ public class CustomerController {
     return ResponseEntity.ok(CustomerDto.from(customer));
   }
 
+  @PostMapping("/balance")
+  public ResponseEntity<Integer> changeBalance(
+      @RequestHeader(name = "X-AUTH-TOKEN") String token,
+      @RequestBody ChangeBalanceForm form
+  ) {
+    UserVo vo = provider.getUserVo(token);
+
+    return ResponseEntity.ok(
+        customerBalanceService.changeBalance(vo.getId(), form).getCurrentBalance()
+    );
+  }
 }
