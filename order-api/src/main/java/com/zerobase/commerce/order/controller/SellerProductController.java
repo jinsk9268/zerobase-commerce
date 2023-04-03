f00997ef332f87bea84ca11e76e2afaf0f688b8e
jinsk9268 <jinsk9268@gmail.com>
2023-04-03 19:48:36 +0900

    feat: 상품, 상품 아이템 수정 PUT API 요청 추가
    - SellerProductController

--- order-api/src/main/java/com/zerobase/commerce/order/controller/SellerProductController.java
+++ order-api/src/main/java/com/zerobase/commerce/order/controller/SellerProductController.java
@@ -3,12 +3,16 @@ package com.zerobase.commerce.order.controller;
 import com.zerobase.commerce.order.domain.product.AddProductForm;
 import com.zerobase.commerce.order.domain.product.AddProductItemForm;
 import com.zerobase.commerce.order.domain.product.ProductDto;
+import com.zerobase.commerce.order.domain.product.ProductItemDto;
+import com.zerobase.commerce.order.domain.product.UpdateProductForm;
+import com.zerobase.commerce.order.domain.product.UpdateProductItemForm;
 import com.zerobase.commerce.order.service.ProductItemService;
 import com.zerobase.commerce.order.service.ProductService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +27,9 @@ public class SellerProductController {
   private final ProductItemService productItemService;
   private final JwtAuthenticationProvider provider;
 
+  /**
+   * 상품 및 아이템 추가
+   */
   @PostMapping
   public ResponseEntity<ProductDto> addProduct(
       @RequestHeader(name = "X-AUTH-TOKEN") String token,
@@ -30,20 +37,54 @@ public class SellerProductController {
   ) {
     return ResponseEntity.ok(
         ProductDto.from(
-            productService.addProduct(provider.getUserVo(token).getId(), form)
+            productService.addProduct(getSellerId(token), form)
         )
     );
   }
 
-  @PostMapping("/items")
+  @PostMapping("/item")
   public ResponseEntity<ProductDto> addProductItem(
       @RequestHeader(name = "X-AUTH-TOKEN") String token,
       @RequestBody AddProductItemForm form
   ) {
     return ResponseEntity.ok(
         ProductDto.from(
-            productItemService.addProductItem(provider.getUserVo(token).getId(), form)
+            productItemService.addProductItem(getSellerId(token), form)
         )
     );
+  }
+
+  /**
+   * 상품 및 아이템 수정
+   */
+  @PutMapping
+  public ResponseEntity<ProductDto> updateProduct(
+      @RequestHeader(name = "X-AUTH-TOKEN") String token,
+      @RequestBody UpdateProductForm form
+  ) {
+    return ResponseEntity.ok(
+        ProductDto.from(
+            productService.updateProduct(getSellerId(token), form)
+        )
+    );
+  }
+
+  @PutMapping("/item")
+  public ResponseEntity<ProductItemDto> updateProductItem(
+      @RequestHeader(name = "X-AUTH-TOKEN") String token,
+      @RequestBody UpdateProductItemForm form
+  ) {
+    return ResponseEntity.ok(
+        ProductItemDto.from(
+            productItemService.updateProductItem(getSellerId(token), form)
+        )
+    );
+  }
+
+  /**
+   * 토큰으로 부터 셀러 id 가져오기
+   */
+  private Long getSellerId(String token) {
+    return provider.getUserVo(token).getId();
   }
 }
