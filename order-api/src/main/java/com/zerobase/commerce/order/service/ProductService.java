11fd2058b16d6802148d412a9043e8c4d40236c0
jinsk9268 <jinsk9268@gmail.com>
2023-04-03 19:47:52 +0900

    feat: 상품 수정 서비스 구현
    - ProductService

--- order-api/src/main/java/com/zerobase/commerce/order/service/ProductService.java
+++ order-api/src/main/java/com/zerobase/commerce/order/service/ProductService.java
@@ -1,8 +1,15 @@
 package com.zerobase.commerce.order.service;
 
+import static com.zerobase.commerce.order.exception.ErrorCode.NOT_FOUND_ITEM;
+import static com.zerobase.commerce.order.exception.ErrorCode.NOT_FOUND_PRODUCT;
+
 import com.zerobase.commerce.order.domain.model.ProductEntity;
+import com.zerobase.commerce.order.domain.model.ProductItemEntity;
 import com.zerobase.commerce.order.domain.product.AddProductForm;
+import com.zerobase.commerce.order.domain.product.UpdateProductForm;
+import com.zerobase.commerce.order.domain.product.UpdateProductItemForm;
 import com.zerobase.commerce.order.domain.repository.ProductRepository;
+import com.zerobase.commerce.order.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +23,26 @@ public class ProductService {
   @Transactional
   public ProductEntity addProduct(Long sellerId, AddProductForm form) {
     return productRepository.save(ProductEntity.of(sellerId, form));
+  }
+
+  @Transactional
+  public ProductEntity updateProduct(Long sellerId, UpdateProductForm form) {
+    ProductEntity product = productRepository.findBySellerIdAndId(sellerId, form.getId())
+        .orElseThrow(() -> new CustomException(NOT_FOUND_PRODUCT));
+    product.setName(form.getName());
+    product.setDescription(form.getDescription());
+
+    for (UpdateProductItemForm itemForm: form.getItems()) {
+      ProductItemEntity item = product.getProductItems().stream()
+          .filter(pi -> pi.getId().equals(itemForm.getId()))
+          .findFirst()
+          .orElseThrow(() -> new CustomException(NOT_FOUND_ITEM));
+
+      item.setName(itemForm.getName());
+      item.setPrice(itemForm.getPrice());
+      item.setCount(itemForm.getCount());
+    }
+
+    return product;
   }
 }
