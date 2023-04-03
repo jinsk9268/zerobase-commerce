afc7e3b6480bb6d00edaf331b3c161896a6c88f0
jinsk9268 <jinsk9268@gmail.com>
2023-04-03 19:47:19 +0900

    feat: 상품 아이템 수정 서비스 구현
    - ProductItemService

--- order-api/src/main/java/com/zerobase/commerce/order/service/ProductItemService.java
+++ order-api/src/main/java/com/zerobase/commerce/order/service/ProductItemService.java
@@ -1,11 +1,14 @@
 package com.zerobase.commerce.order.service;
 
+import static com.zerobase.commerce.order.exception.ErrorCode.NOT_FOUND_ITEM;
 import static com.zerobase.commerce.order.exception.ErrorCode.NOT_FOUND_PRODUCT;
 import static com.zerobase.commerce.order.exception.ErrorCode.SAME_ITEM_NAME;
 
 import com.zerobase.commerce.order.domain.model.ProductEntity;
 import com.zerobase.commerce.order.domain.model.ProductItemEntity;
 import com.zerobase.commerce.order.domain.product.AddProductItemForm;
+import com.zerobase.commerce.order.domain.product.UpdateProductItemForm;
+import com.zerobase.commerce.order.domain.repository.ProductItemRepository;
 import com.zerobase.commerce.order.domain.repository.ProductRepository;
 import com.zerobase.commerce.order.exception.CustomException;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductItemService {
 
   private final ProductRepository productRepository;
+  private final ProductItemRepository productItemRepository;
 
   @Transactional
   public ProductEntity addProductItem(Long sellerId, AddProductItemForm form) {
@@ -33,5 +37,18 @@ public class ProductItemService {
     product.getProductItems().add(productItem);
 
     return product;
+  }
+
+  @Transactional
+  public ProductItemEntity updateProductItem(Long sellerId, UpdateProductItemForm form) {
+    ProductItemEntity productItem = productItemRepository.findById(form.getId())
+        .filter(pi -> pi.getSellerId().equals(sellerId))
+        .orElseThrow(() -> new CustomException(NOT_FOUND_ITEM));
+
+    productItem.setName(form.getName());
+    productItem.setCount(form.getCount());
+    productItem.setPrice(form.getPrice());
+
+    return productItem;
   }
 }
