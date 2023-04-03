5d2a6819faee06e85de2d0a51251f9568946aa15
jinsk9268 <jinsk9268@gmail.com>
2023-04-03 11:30:01 +0900

    feat: 상품 찾기 불가, 중복 아이템 에러 코드 추가
    - ErrorCode

--- order-api/src/main/java/com/zerobase/commerce/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zerobase/commerce/order/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-  SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다.");
+  NOT_FOUND_PRODUCT(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다."),
+  SAME_ITEM_NAME(HttpStatus.BAD_REQUEST, "중복된 아이템명입니다.");
 
   private final HttpStatus httpStatus;
   private final String errorMessage;
