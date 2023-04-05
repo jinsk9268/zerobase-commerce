44bf83236554a28c22c2de98b537bdad6ca74245
jinsk9268 <jinsk9268@gmail.com>
2023-04-05 15:15:55 +0900

    feat: 장바구니 가져오기, 추가 서비스 구현
    - CartService

---
+++ order-api/src/main/java/com/zerobase/commerce/order/service/CartService.java
@@ -0,0 +1,73 @@
+package com.zerobase.commerce.order.service;
+
+import com.zerobase.commerce.order.client.RedisClient;
+import com.zerobase.commerce.order.domain.product.AddProductCartForm;
+import com.zerobase.commerce.order.domain.redis.Cart;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CartService {
+
+  private final RedisClient redisClient;
+
+  public Cart getCart(Long customerId) {
+    return redisClient.get(customerId, Cart.class);
+  }
+
+  public Cart addCart(Long customerId, AddProductCartForm form) {
+    Cart cart = getCart(customerId);
+
+    if (cart == null) {
+      cart = new Cart();
+      cart.setCustomerId(customerId);
+    }
+
+    // 같은 상품이 있는지 확인
+    Optional<Cart.Product> productOptional = cart.getProducts().stream()
+        .filter(p -> p.getId().equals(form.getId()))
+        .findFirst();
+
+    if (productOptional.isPresent()) {
+      Cart.Product redisProduct = productOptional.get();
+
+      // requested
+      List<Cart.ProductItem> items = form.getItems().stream()
+          .map(Cart.ProductItem::from).collect(Collectors.toList());
+
+      Map<Long, Cart.ProductItem> redisItemMap = redisProduct.getItems().stream()
+          .collect(Collectors.toMap(Cart.ProductItem::getId, it -> it));
+
+      if (!redisProduct.getName().equals(form.getName())) {
+        cart.addMessage(redisProduct.getName() + "의 정보가 변경되었습니다. 확인 부탁드립ㄴ디ㅏ.");
+      }
+
+      for (Cart.ProductItem item : items) {
+        Cart.ProductItem redisItem = redisItemMap.get(item.getId());
+
+        if (redisItem == null) {
+          redisProduct.getItems().add(item);
+        } else {
+          if (!redisItem.getPrice().equals(item.getPrice())) {
+            cart.addMessage(redisProduct.getName() + " " + item.getName() +
+                "의 가격이 변경되었습니다. 확인 부탁드립니다.");
+          }
+        }
+      }
+    } else {
+      Cart.Product product = Cart.Product.from(form);
+      cart.getProducts().add(product);
+    }
+
+    redisClient.put(customerId, cart);
+
+    return cart;
+  }
+}
