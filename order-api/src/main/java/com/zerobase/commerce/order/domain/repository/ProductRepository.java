14c033d7a00cdab4ed224e3789d280c7effea6a3
jinsk9268 <jinsk9268@gmail.com>
2023-04-06 10:07:10 +0900

    feat: 상품 아이디 리스트로 상품 조회 쿼리 추가
    - ProductRepository

--- order-api/src/main/java/com/zerobase/commerce/order/domain/repository/ProductRepository.java
+++ order-api/src/main/java/com/zerobase/commerce/order/domain/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.zerobase.commerce.order.domain.repository;
 
 import com.zerobase.commerce.order.domain.model.ProductEntity;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
@@ -8,9 +9,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository
     extends JpaRepository<ProductEntity, Long>, ProductRepositoryCustom {
+
   @EntityGraph(attributePaths = {"productItems"}, type = EntityGraphType.LOAD)
   Optional<ProductEntity> findWithProductItemsById(Long id);
 
   @EntityGraph(attributePaths = {"productItems"}, type = EntityGraphType.LOAD)
   Optional<ProductEntity> findBySellerIdAndId(Long sellerId, Long id);
+
+  @EntityGraph(attributePaths = {"productItems"}, type = EntityGraphType.LOAD)
+  List<ProductEntity> findAllByIdIn(List<Long> ids);
 }
