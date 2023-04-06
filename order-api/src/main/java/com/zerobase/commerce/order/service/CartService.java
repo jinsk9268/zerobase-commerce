09ae5d5ef4933fb99de847da706c7394e9548f10
jinsk9268 <jinsk9268@gmail.com>
2023-04-06 10:17:48 +0900

    feat: 카드 가져오기, 수정하기 서비스 구현
    - CartService

--- order-api/src/main/java/com/zerobase/commerce/order/service/CartService.java
+++ order-api/src/main/java/com/zerobase/commerce/order/service/CartService.java
@@ -19,16 +19,18 @@ public class CartService {
   private final RedisClient redisClient;
 
   public Cart getCart(Long customerId) {
-    return redisClient.get(customerId, Cart.class);
+    Cart cart = redisClient.get(customerId, Cart.class);
+    return cart != null ? cart : new Cart(customerId);
+  }
+
+  public Cart putCart(Long customerId, Cart cart) {
+    redisClient.put(customerId, cart);
+
+    return cart;
   }
 
   public Cart addCart(Long customerId, AddProductCartForm form) {
     Cart cart = getCart(customerId);
-
-    if (cart == null) {
-      cart = new Cart();
-      cart.setCustomerId(customerId);
-    }
 
     // 같은 상품이 있는지 확인
     Optional<Cart.Product> productOptional = cart.getProducts().stream()
@@ -46,7 +48,7 @@ public class CartService {
           .collect(Collectors.toMap(Cart.ProductItem::getId, it -> it));
 
       if (!redisProduct.getName().equals(form.getName())) {
-        cart.addMessage(redisProduct.getName() + "의 정보가 변경되었습니다. 확인 부탁드립ㄴ디ㅏ.");
+        cart.addMessage(redisProduct.getName() + "의 정보가 변경되었습니다. 확인 부탁드립니다.");
       }
 
       for (Cart.ProductItem item : items) {
